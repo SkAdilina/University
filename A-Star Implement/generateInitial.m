0a842cc513290b7839ee43e359283bb032cda66f
SkAdilina <31650090+SkAdilina@users.noreply.github.com>
2017-10-03 15:04:34 +0600

    delete

--- A-Star Implement/generateInitial.m
+++
@@ -1,5 +0,0 @@
-function c=generateInitial()
-% create a initial node here, c will be a vector that contains 9 elements
-c=[1 3 6 2 7 8 5 4 0];
-
-end
--- A-Star Implement/stepCost.m
+++
@@ -1,3 +0,0 @@
-function cost=stepCost(action)
-	cost=1; % every problem wont have the same action costs
-end
